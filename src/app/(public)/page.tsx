bbe7bfd4ae12f5dcdbb78d1f1f6013453ac391fc
NaeemAfridi <coder3351@gmail.com>
2025-10-24 12:31:47 +0500

    app files and folder structure implemented

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to School Management System
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Empowering schools, teachers, and students through technology.
+      </p>
+    </main>
+  );
+}
